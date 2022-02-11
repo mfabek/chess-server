b803d5099b196f05d4f840cd7e6cf7d3d9cf9317
Matija Fabek <matija.fabek@ericsson.com>
2022-02-11 15:08:01 +0100

    Websocket configuration

--- src/main/java/com/pmf/chessgame/config/WebSocketConfig.java
+++ src/main/java/com/pmf/chessgame/config/WebSocketConfig.java
@@ -1,2 +1,25 @@
-package com.pmf.chessgame.config;public class WebSocketConfig {
+package com.pmf.chessgame.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/chess");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry
+                .addEndpoint("/socket")
+                .setAllowedOrigins("http://localhost:4200")
+                .withSockJS();
+    }
 }
